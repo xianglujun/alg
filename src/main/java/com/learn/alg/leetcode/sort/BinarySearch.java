724068917dada90252f255ab11f99ed9992f8db1
xianglujun <xianglj1991@163.com>
2022-04-11 23:31:28 +0800

    leetcode 关于二分查找代码实现

---
+++ src/main/java/com/learn/alg/leetcode/sort/BinarySearch.java
@@ -0,0 +1,32 @@
+package com.learn.alg.leetcode.sort;
+
+/**
+ * 第704题，实现二分查找
+ * <pre>
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * </pre>
+ */
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > nums[mid]) {
+                start = mid + 1;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        int index = new BinarySearch().search(nums, 9);
+        System.out.println(index);
+    }
+}
